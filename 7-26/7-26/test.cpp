67c6520343d885a23e3b9f157a706ac2c2233eb8
UchihaMadara1 <2898831413@qq.com>
2020-07-26 23:27:53 +0800

    “练习”

---
+++ 7-26/7-26/test.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+//int main()
+//{
+//	string str1;
+//	string str2;
+//	while (getline(cin, str1))
+//	{
+//		//str.erase(0,3);
+//		if (str1.size() == 0)
+//			break;
+//		getline(cin,str2);
+//		for (auto i = str1.begin(); i != str1.end(); ++i)
+//		{
+//			unsigned int it = str2.find(*i);
+//			if (it != string::npos)
+//				str1.erase(i);
+//		}
+//		cout << str1 << endl;
+//	}
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
