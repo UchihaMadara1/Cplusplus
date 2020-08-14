ce81aa8ae1e4508092f32add87731741d3b764d2
UchihaMadara1 <2898831413@qq.com>
2020-08-14 23:40:30 +0800

    “练习”

---
+++ 8-14/8-14/test.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	//int i, j, sum;
+	//for (i = 1; i < 1001; ++i)
+	//{
+	//	sum = 0;
+	//	for (j = 2; j < sqrt(i); ++j)
+	//	{
+	//		if (i % j == 0)
+	//		{
+	//			sum += j;
+	//			sum += i / j;
+	//		}
+	//	}
+	//	if (sum  + 1 == i && i != 1)
+	//		printf("%5d",i);
+	//}
+	//return 0;
+	/*int i = 0;
+	if (i)
+		printf("haha\n");;
+	else
+		printf("heihei\n");*/
+}
